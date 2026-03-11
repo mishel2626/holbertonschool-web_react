b22b4b945f148cce697267730051010f5dfc425c
mishel2626 <mishelspaho@gmail.com>
2026-03-11 17:35:49 +0100

    commit

--- react_intro/task_2/dashboard/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsxx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
